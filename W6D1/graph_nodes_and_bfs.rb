6e68fc6541cb0fdbfaf2f674e4848004d4170bf0
martmark <martinmarkaj@live.com>
2019-02-10 23:49:10 -0500

    Create GraphNode class

--- W6D1/graph_nodes_and_bfs.rb
+++ W6D1/graph_nodes_and_bfs.rb
@@ -6,4 +6,22 @@ class GraphNode
         self.neighbors = []
     end
 
+    def bfs(starting_node, target_value)
+
+
+    end
+
 end
+
+
+a = GraphNode.new('a')
+b = GraphNode.new('b')
+c = GraphNode.new('c')
+d = GraphNode.new('d')
+e = GraphNode.new('e')
+f = GraphNode.new('f')
+a.neighbors = [b, c, e]
+c.neighbors = [b, d]
+e.neighbors = [a]
+f.neighbors = [e]
+
