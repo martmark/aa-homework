5b8f30be49ceed4b42b82a392a442c791a204e1e
martmark <martinmarkaj@live.com>
2019-02-13 08:45:08 -0500

    Make simon game runnable

--- W6D3/simon/lib/simon.rb
+++ W6D3/simon/lib/simon.rb
@@ -1,41 +1,72 @@
+#TO PLAY JUST RUN 'ruby simon.rb'
+
 class Simon
   COLORS = %w(red blue green yellow)
 
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 
   def play
-
+    self.take_turn until game_over
+    if game_over
+      self.game_over_message
+      self.reset_game
+    end
+    
   end
 
   def take_turn
-
+    self.show_sequence
+    sleep 2
+    system("clear")
+    self.require_sequence
+    unless @game_over
+      self.round_success_message
+      @sequence_length += 1
+    end
   end
 
   def show_sequence
-
+    self.add_random_color
+    puts seq
   end
 
   def require_sequence
-
+    current_seq = @seq.dup
+    until current_seq.empty? || @game_over
+      current = current_seq.shift
+      guess = gets.chomp.downcase
+      if guess == current
+        next
+      else
+        @game_over = true
+      end
+    end
   end
 
   def add_random_color
-
+    @seq << COLORS.sample
   end
 
   def round_success_message
-
+    puts "Well done. Let's keep going..."
   end
 
   def game_over_message
-
+    puts "You lose. Do some brain exercises then play again."
   end
 
   def reset_game
-
+    self.sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 end
+
+new = Simon.new
+new.play
