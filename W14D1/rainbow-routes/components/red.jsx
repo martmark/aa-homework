6dba618fbea79ec12fbb844688b4f6ca829dfd76
martmark <martinmarkaj@live.com>
2019-04-08 10:56:27 -0400

    added w14d1

---
+++ W14D1/rainbow-routes/components/red.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Route, Link, NavLink } from 'react-router-dom';
+import Orange from './orange';
+import Yellow from './yellow';
+
+class Red extends React.Component {
+  render() {
+    return(
+      <div>
+        <h2 className="red"></h2>
+        <Link to='/red' >Red only</Link>
+        <Link to='/red/orange' >Add orange</Link>
+        <Link to='/red/yellow' >Add yellow</Link>
+
+        <Route path="/red/orange" component={Orange} />
+        <Route path="/red/yellow" component={Yellow} />
+      </div>
+    );
+  }
+};
+
+export default Red;
